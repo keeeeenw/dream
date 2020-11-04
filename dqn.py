b25e3859a5d14524d8532580838562c0c3598eb5
Zixiao Wang <wangxiaotian2007@gmail.com>
2020-11-04 06:07:40 -0800

    dream_delayed_train_gan_params_reward_relabel
    Separate Generator and Discriminator
    Delayed use_ids False
    Applying reward relabeling

--- dqn.py
+++ dqn.py
@@ -360,20 +360,19 @@ class RecurrentDQNPolicy(DQNPolicy):
     # TODO(evzliu): Could more gracefully handle this by passing a
     # TrajectoryExperience object to label_rewards to take TrajectoryExperience
     # Relabel the rewards on the fly
-    # Do not relabel reward for GAN
-    # if self._reward_relabeler is not None:
-    #   trajectories = [seq[0].trajectory for seq in experiences]
-    #   # (batch_size, max_seq_len)
-    #   indices = torch.tensor(
-    #       [[e.index for e in seq] for seq in experiences]).long()
+    if self._reward_relabeler is not None:
+      trajectories = [seq[0].trajectory for seq in experiences]
+      # (batch_size, max_seq_len)
+      indices = torch.tensor(
+          [[e.index for e in seq] for seq in experiences]).long()
       
-    #   random_trajectories = None
-    #   if random_experiences is not None:
-    #     random_trajectories = [seq[0].trajectory for seq in random_experiences]
+      random_trajectories = None
+      if random_experiences is not None:
+        random_trajectories = [seq[0].trajectory for seq in random_experiences]
 
-    #   # (batch_size * max_trajectory_len)
-    #   rewards = self._reward_relabeler.label_rewards(
-    #       trajectories, random_trajectories)[0].gather(-1, indices).reshape(-1)
+      # (batch_size * max_trajectory_len)
+      rewards = self._reward_relabeler.label_rewards(
+          trajectories, random_trajectories)[0].gather(-1, indices).reshape(-1)
 
     # (batch_size,) 1 if was not done, otherwise 0
     not_done_mask = ~(torch.tensor(
